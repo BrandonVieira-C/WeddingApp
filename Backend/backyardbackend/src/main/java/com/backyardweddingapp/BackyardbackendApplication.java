d35b395ec3984c5dc1cbdb33d8699d8af1b35f50
Nubbynub <garengh4@gmail.com>
2022-04-07 16:53:15 -0400

    cookie adds (#3)

--- Backend/backyardbackend/src/main/java/com/backyardweddingapp/BackyardbackendApplication.java
+++ Backend/backyardbackend/src/main/java/com/backyardweddingapp/BackyardbackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackyardbackendApplication {
-
+// test test test from cookie branch
 	public static void main(String[] args) {
 		SpringApplication.run(BackyardbackendApplication.class, args);
 		
