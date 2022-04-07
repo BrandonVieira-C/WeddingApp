13099c3b423a6f390a3576cfb0519b7af175de90
BrandonVieira-C <brandon.c.vieira@gmail.com>
2022-04-07 15:54:09 -0400

    updated service classes for customer components

--- Frontend/backyardfrontend/src/app/login/customer-login/customer-login.service.ts
+++ Frontend/backyardfrontend/src/app/login/customer-login/customer-login.service.ts
@@ -2,18 +2,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from 'src/app/shared/entity/Customer';
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerLoginService {
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  private baseUrl = "http://localhost:8080/wedding";
   
   constructor(private http: HttpClient) { }
 
   public login(customer: Customer): Observable<Customer> {
-    return this.http.post<Customer>((this.baseUrl+"/customerlogin"), customer, {headers: this.headers});
+    return this.http.post<Customer>((environment.customerApiUrl+"/customerlogin"), customer, {headers: this.headers});
 
   }
 }
